604427c932589596a5b05e3fb3269a424ea45436
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-01-16 21:45:34 +0100

    port

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 buildRoutes(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
     console.log(`Listening on ${port}...`);
